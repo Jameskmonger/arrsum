78aee96b5a76d28a91abb3019a6acee44029d7c0
James Monger <jameskmonger@hotmail.co.uk>
2016-04-02 01:17:08 +0100

    Check for 0 when missing properties

--- arrsum.spec.js
+++ arrsum.spec.js
@@ -10,3 +10,16 @@ test('it returns 0 for empty array', t => {
 
     t.assert.equal(0, total);
 });
+
+test('it returns 0 if property doesn\'t exist', (t, input, property) => {
+    let total = arrsum(input, property);
+
+    t.assert.equal(0, total);
+}, [
+    [
+        [ { value: 3 }, { value: 2} ], 'nonexistent'
+    ],
+    [
+        [ { count: 17 }, { count: 17 } ], 'also-non-existent'
+    ]
+]);
