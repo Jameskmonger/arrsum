a26e6d523420029589e4368f15645db5f18164a7
James Monger <jameskmonger@hotmail.co.uk>
2016-04-02 01:24:28 +0100

    Pass test

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function (array, property) {
+var forkeys = require('forkeys-compat');
 
+module.exports = function (array, property) {
+    var total = 0;
+
+    array.forEach(function (obj) {
+        total += (obj[property] || 0);
+    });
+
+    return total;
 };
